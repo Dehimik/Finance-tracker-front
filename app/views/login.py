7267189e8f10a2e7ecda367ecd9cb87641e00253
Dehimik <vladkepkin@gmail.com>
2025-02-02 13:20:06 +0200

    Add logic to login and register

---
+++ app/views/login.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import messagebox
+import requests
+
+from app.services.api_client import login_request
+
+class LoginWindow(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        tk.Label(self, text="Nickname").pack()
+        self.nickname_entry = tk.Entry(self)
+        self.nickname_entry.pack()
+
+        tk.Label(self, text="Password").pack()
+        self.password_entry = tk.Entry(self, show="*")
+        self.password_entry.pack()
+
+        tk.Button(self, text="Login", command=self.login).pack()
+
+    def login(self):
+        nickname = self.nicknameentry.get()
+        password = self.password_entry.get()
+
+        if not nickname or not password:
+            messagebox.showwarning("Error", "Fill all fields!")
+            return
+
+        try:
+            response = login_request(nickname, password)
+
+            if "message" in response:
+                messagebox.showinfo("Success", "Login successfully!")
+            else:
+                messagebox.showerror("Error", "Incorrect nickname or password!")
+        except requests.exceptions.RequestException as e:
+            messagebox.showerror("Error", f"I cant move it move it anymore: {e}")
---
+++ app/views/register.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import messagebox
+import requests
+
+from app.services.api_client import register_request
+
+class RegisterWindow(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+    def create_widgets(self):
+        tk.Label(self, text="Email").pack()
+        self.email_entry = tk.Entry(self)
+        self.email_entry.pack()
+
+        tk.Label(self, text="Nickname").pack()
+        self.nickname_entry = tk.Entry(self)
+        self.nickname_entry.pack()
+
+        tk.Label(self, text="Password").pack()
+        self.password_entry = tk.Entry(self, show="*")
+        self.password_entry.pack()
+
+        tk.Label(self, text="Retype password").pack()
+        self.confirm_password_entry = tk.Entry(self, show="*")
+        self.confirm_password_entry.pack()
+        tk.Button(self, text="Register", command=self.register).pack()
+    def register(self):
+        email = self.email_entry.get()
+        nickname = self.nickname_entry.get()
+        password = self.password_entry.get()
+        confirm_password = self.confirm_password_entry.get()
+
+        if not email or not nickname or not password or not confirm_password:
+            messagebox.showwarning("Error", "Fill all fields!")
+            return
+        if password != confirm_password:
+            messagebox.showerror("Error", "Passwords doesnt match!")
+            return
+        try:
+            response = register_request(email, nickname, password)
+
+            if "message" in response:
+                messagebox.showinfo("Success", "Register success! Login now.")
+            else:
+                messagebox.showerror("Error", "Email already exists!")
+        except requests.exceptions.RequestException as e:
+            messagebox.showerror("Error", f"Registration unsuccessfully: {e}")
